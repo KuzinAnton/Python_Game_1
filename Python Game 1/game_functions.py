54cd2f39f3bd899fc380cfa42c2a30b8dab2e1e1
KuzinAnton <anton_kuzin@mail.ru>
2020-01-28 16:42:39 +0300

    Рефакторинг check_events

--- Python Game 1/game_functions.py
+++ Python Game 1/game_functions.py
@@ -1,24 +1,30 @@
 import sys
 import pygame
 
+def check_keydown_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+        
+def check_keyup_events(event, ship):
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    if event.key == pygame.K_LEFT:
+        ship.moving_left = False
+
+
 def check_events(ship):
     #Отслеживание событий клавиатуры и мыши.
     for event in pygame.event.get():
-       if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:
             pygame.quit()
             #try:
             sys.exit(0)
-       elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
-
-       elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            if event.key == pygame.K_LEFT:
-                ship.moving_left = False
+        elif event.type == pygame.KEYDOWN:
+            check_keydown_events(event, ship)
+        elif event.type == pygame.KEYUP:
+            check_keyup_events(event, ship)
             #except:
            #     print('Error in exit')
             #    return
