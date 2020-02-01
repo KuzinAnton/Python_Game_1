868eb4908b50e80acbb8c481676ff6e5d3c58baf
KuzinAnton <anton_kuzin@mail.ru>
2020-02-01 23:30:18 +0300

    Добавленик кнопки Play

--- Python Game 1/alien_invasion.py
+++ Python Game 1/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from game_stats import GameStats
+from button import Button
 import game_functions as gf
 
 def run_game():
@@ -11,6 +12,8 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode((ai_settings.screen_width,ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
+    #Создание кнопки Play
+    play_button = Button(ai_settings,screen,'Play')
     #Создание экземпляра для хранения игровой статистики
     stats = GameStats(ai_settings)
     #Создание корабля
@@ -24,13 +27,13 @@ def run_game():
     
     #Запуск основного цикла игры
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, bullets)
 
         if stats.game_active:
             ship.update()
             gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
             gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
 
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
 
 run_game()
