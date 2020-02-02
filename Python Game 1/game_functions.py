4657a426e1722702cd513ad19ed50421bfa89723
KuzinAnton <anton_kuzin@mail.ru>
2020-02-02 16:49:43 +0300

    Добавлено изменение скорости

--- Python Game 1/game_functions.py
+++ Python Game 1/game_functions.py
@@ -55,6 +55,9 @@ def check_play_button(ai_settings, screen, stats, play_button, ship, aliens, bul
     """Запускает новую игру при нажатии кнокпи Play."""
     button_clicked = play_button.rect.collidepoint(mouse_x, mouse_y)
     if button_clicked and not stats.game_active:
+        #Сброс игровых настроек
+        ai_settings.initialize_dynamic_settings()
+
         #Указатель мыши скрывается.
         pygame.mouse.set_visible(False)
 
@@ -105,8 +108,9 @@ def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
     #Удаление пуль и пришельцев, участвующих в коллизиях
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
     if len(aliens) == 0:
-        #Уничтожение существующих пуль и создание нового флота
+        #Уничтожение существующих пуль, повышение скорости и создание нового флота
         bullets.empty()
+        ai_settings.increase_speed()
         create_fleet(ai_settings, screen, ship, aliens)
 
 def get_number_aliens_x(ai_settings, alien_width):
